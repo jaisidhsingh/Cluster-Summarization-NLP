047255b7a7755d023c013c869839eacfc8c417b2
Jaisidh Singh <75247817+jaisidhsingh@users.noreply.github.com>
2022-11-08 09:51:22 +0530

    Update load_examples.py

--- src/load_examples.py
+++ src/load_examples.py
@@ -6,7 +6,7 @@ def example_set(set=1):
     assert set == 1, f'Set{set} was not found among the provided example sets'
 
     articles = []
-    path = f"../examples/set{set}/"
+    path = f"examples/set{set}/"
     for file in os.listdir(path):
         f = open(path+file, "r", encoding='utf8')
         data = f.read()
