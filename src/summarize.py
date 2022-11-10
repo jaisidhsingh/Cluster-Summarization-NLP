ac40e5c609a7cdfe2d6da36ba779deffcd2f5591
Jaisidh Singh <75247817+jaisidhsingh@users.noreply.github.com>
2022-11-10 20:23:41 +0530

    Update summarize.py

--- src/summarize.py
+++ src/summarize.py
@@ -6,8 +6,12 @@ import argparse
 import warnings
 warnings.filterwarnings("ignore")
 
-
 def get_summary(articles, threshold):
+	EMBEDDING_MODEL = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+	SUMMARIZATION_TOKENIZER = BartTokenizer.from_pretrained(
+	    'facebook/bart-large-cnn')
+	SUMMARIZATION_MODEL = BartForConditionalGeneration.from_pretrained(
+	    'facebook/bart-large-cnn')
 	sentences, words = make_data(articles)
 	embeddings = sentence2embedding(sentences, EMBEDDING_MODEL)
 
