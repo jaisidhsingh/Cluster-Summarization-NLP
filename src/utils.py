5390c690212dd1266bcbdd85a0907c6ecddd97a7
Jaisidh Singh <75247817+jaisidhsingh@users.noreply.github.com>
2022-11-10 20:17:36 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -14,37 +14,6 @@ from sentence_transformers import SentenceTransformer
 warnings.filterwarnings("ignore")
 
 
-
-def get_summary(articles, threshold):
-	EMBEDDING_MODEL = SentenceTransformer('paraphrase-MiniLM-L6-v2')
-	SUMMARIZATION_TOKENIZER = BartTokenizer.from_pretrained(
-	    'facebook/bart-large-cnn')
-	SUMMARIZATION_MODEL = BartForConditionalGeneration.from_pretrained(
-	    'facebook/bart-large-cnn')
-	sentences, words = make_data(articles)
-	embeddings = sentence2embedding(sentences, EMBEDDING_MODEL)
-
-	CUSTOM_CLEMB_KWARGS = {
-		'clustering_algorithm': AgglomerativeClustering(
-			n_clusters=None,
-			distance_threshold=threshold
-		),
-		'embeddings': embeddings,
-		'sentences': sentences,
-		'words': words,
-		'num_articles': len(articles)
-	}
-
-	clemb = ClusterSentenceEmbeddings(**CUSTOM_CLEMB_KWARGS)
-	sentence_clusters = clemb.get_sentence_clusters()
-
-	final_summary = ""
-	for cluster in sentence_clusters:
-		summary = bart_summarize(cluster, SUMMARIZATION_MODEL, SUMMARIZATION_TOKENIZER)
-		final_summary += summary + " "
-
-	return final_summary
-
 def make_data(articles):
     sentences = []
     words = []
@@ -187,3 +156,33 @@ class ClusterSentenceEmbeddings():
             chunk = ""
 
         return np.array(indiv_clusters)
+
+def get_summary(articles, threshold):
+	EMBEDDING_MODEL = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+	SUMMARIZATION_TOKENIZER = BartTokenizer.from_pretrained(
+	    'facebook/bart-large-cnn')
+	SUMMARIZATION_MODEL = BartForConditionalGeneration.from_pretrained(
+	    'facebook/bart-large-cnn')
+	sentences, words = make_data(articles)
+	embeddings = sentence2embedding(sentences, EMBEDDING_MODEL)
+
+	CUSTOM_CLEMB_KWARGS = {
+		'clustering_algorithm': AgglomerativeClustering(
+			n_clusters=None,
+			distance_threshold=threshold
+		),
+		'embeddings': embeddings,
+		'sentences': sentences,
+		'words': words,
+		'num_articles': len(articles)
+	}
+
+	clemb = ClusterSentenceEmbeddings(**CUSTOM_CLEMB_KWARGS)
+	sentence_clusters = clemb.get_sentence_clusters()
+
+	final_summary = ""
+	for cluster in sentence_clusters:
+		summary = bart_summarize(cluster, SUMMARIZATION_MODEL, SUMMARIZATION_TOKENIZER)
+		final_summary += summary + " "
+
+	return final_summary
